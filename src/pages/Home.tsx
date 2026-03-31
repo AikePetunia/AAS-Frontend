2398f177c6fa208b248b6237a8e800801d909162
Aike <dvhb47487963@gmail.com>
2026-03-31 14:29:34 -0300

    @

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "@components/common/Navbar/Navbar";
 import { MainSection } from "@components/MainSection/MainSection";
 import { FeaturedProducts } from "@components/FeaturedProducts/FeaturedProducts";
 import { StoresList } from "@components/StoresList/StoresList";
-import { Stats } from "@components/Stats/Stats" 
+import { Stats } from "@components/Stats/Stats";
 
 export function Home() {
   return (
